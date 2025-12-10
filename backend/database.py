db5b1066f8fa1610bea0ab9f96b56bf8cd87584c
AlexJ683 <alex.johnson106@icloud.com>
2025-12-10 16:02:16 +0000

    updates to python

--- backend/database.py
+++ backend/database.py
@@ -2,11 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-URL_Database =  "mysql+pymysql://root:Password@mysql:3306/planes"
+URL_Database = "mysql+pymysql://root:Password@mysql:3306/planes"
 
 engine = create_engine(URL_Database)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-
-
