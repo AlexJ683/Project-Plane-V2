4553254ab052099b319255aeea757c1f2feacac6
AlexJ683 <alex.johnson106@icloud.com>
2025-12-10 22:08:34 +0000

    wow

---
+++ testing/test_frontend.py
@@ -0,0 +1,13 @@
+import pytest
+from  frontend.Frontend import Data_processing 
+
+@pytest.fixture
+def data_processing():
+    "creates a fresh instance of the class before every time"
+    return Data_processing()
+
+def test_get_column_types():
+    assert Data_processing.get_column_types() == dict
+
+def test_check_data():
+    assert Data_processing.check_data(df) == df
